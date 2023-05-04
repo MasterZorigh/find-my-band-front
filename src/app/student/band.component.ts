2c20adf76614a6dcf7ea248641f196ed78c7cfa4
MasterZorigh <73075421+MasterZorigh@users.noreply.github.com>
2023-05-04 23:40:11 +0200

    fix() : new config

---
+++ src/app/student/band.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-band',
+  templateUrl: './band.component.html',
+  styleUrls: ['./band.component.sass'],
+})
+export class BandComponent {}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  findMyBandBaseUrl: 'http://localhost:3000',
+};
