c92e228946da383a3d71b424d060ea68edc91098
MasterZorigh <73075421+MasterZorigh@users.noreply.github.com>
2023-05-05 09:54:38 +0200

    fix() : clean up all the repo

--- src/app/student/band.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-band',
-  templateUrl: './band.component.html',
-  styleUrls: ['./band.component.sass'],
-})
-export class BandComponent {}
--- src/app/student/models/band.ts
+++
@@ -1,9 +0,0 @@
-export interface Band {
-  id: number;
-  bandName: string;
-  bandCountry: string;
-  bandGenre: string;
-  bandMembers: string;
-  dateOfBirth: string;
-  bandTopSong: string;
-}
